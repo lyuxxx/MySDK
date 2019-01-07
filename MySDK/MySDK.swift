f1998042ab040d18e3c9ba5f7912368b3727e59f
yxliu <yuxuan.liu2@pactera.com>
2019-01-07 16:23:16 +0800

    initial commit

---
+++ MySDK/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by yxliu on 2019/1/7.
+//  Copyright © 2019 cusc. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg)
+    }
+}
